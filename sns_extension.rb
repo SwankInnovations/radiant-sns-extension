4711a07f02a2d0010b45fd97e8337ded66823722
cristi <cristi.duma@aissac.ro>
2009-07-03 15:17:51 +0800

    updated readme and added TODO: Remove directory config entry since Rack cache does not use it.
    Signed-off-by: Sean Cribbs <seancribbs@gmail.com>

--- sns_extension.rb
+++ sns_extension.rb
@@ -3,7 +3,7 @@
   # (relative to RAILS_ROOT). The default value is: "text_asset_cache"
   #
   # NOTE: If you change this, don't forget to remove any previous cache folder
-  # TEXT_ASSET_CACHE_DIR = "text_asset_cache"
+  TEXT_ASSET_CACHE_DIR = "text_asset_cache"
 
 require_dependency 'application_controller'
 require 'ostruct'
